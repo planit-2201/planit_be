95b1d81d361ca334b561f3541d60d5b85a344b2a
Kim G <xiaole.guo@yahoo.com>
2022-07-08 15:53:12 -0600

    setup graphql scaffold

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,19 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+
+    field :get_users, [Types::UserType], null: false, description: 'Returns all users'
+
+    def get_users
+      User.all
+    end
+
+  end
+end
