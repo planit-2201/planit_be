f757eabb526fbfaa4a5cb62ef0f9701cadc6549d
Kim G <xiaole.guo@yahoo.com>
2022-07-08 15:51:53 -0600

    add user and daily records table and seed data

---
+++ db/migrate/20220708212355_add_user_to_daily_records.rb
@@ -0,0 +1,5 @@
+class AddUserToDailyRecords < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :daily_records, :user, null: false, foreign_key: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+scott = User.create!(
+  username: "Scott Drybread",
+  flowrate: 1.8
+)
+
+scott.daily_records.create!(
+  date: '2022-07-07',
+  bag_count: 3,
+  straw_count: 0,
+  container_count: 2,
+  shower_time: 0
+)
+scott.daily_records.create!(
+  date: '2022-07-08',
+  bag_count: 3,
+  straw_count: 0,
+  container_count: 2,
+  shower_time: 0
+)
