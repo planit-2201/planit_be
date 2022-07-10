f6537715b5b14eff6be7122612619c98be1e02a3
Chris Romero <chrisleeromero1@gmail.com>
2022-07-10 15:17:13 -0600

    update daily_record should update a user's daily record

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     # TODO: remove me
     field :create_daily_record, mutation: Mutations::DailyRecords::CreateDailyRecord
+    field :update_daily_record, mutation: Mutations::DailyRecords::UpdateDailyRecord
     field :create_user, mutation: Mutations::Users::CreateUser
     field :update_user, mutation: Mutations::Users::UpdateUser
   end
