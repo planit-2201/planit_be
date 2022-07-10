1e86f87f5c0034688e697fb8299243c4c98524d4
Kim G <xiaole.guo@yahoo.com>
2022-07-10 11:46:54 -0600

    fix getUserDailyRecords tests

--- spec/graphql/queries/users/get_user_daily_records_spec.rb
+++ spec/graphql/queries/users/get_user_daily_records_spec.rb
@@ -5,26 +5,26 @@ RSpec.describe Types::QueryType do
     it "can query a user daily records" do
       user = create(:user, id: 1, username: "Mike Dao", flowrate: 1.8)
 
-      user.daily_records.create(:daily_records, date: "2022-07-09", bag_count: 3, container_count: 4, straw_count: 6, shower_time: 0)
+      user.daily_records.create(date: "2022-07-09", bag_count: 3, container_count: 4, straw_count: 6, shower_time: 0)
       result = PlanitBeSchema.execute(query).as_json
 
-      expect(result['data']['getUserDailyRecords']['date']).to eq("2022-07-09")
-      expect(result['data']['getUserDailyRecords']['bag_count']).to eq(3)
-      expect(result['data']['getUserDailyRecords']['container_count']).to eq(4)
-      expect(result['data']['getUserDailyRecords']['straw_count']).to eq(6)
-      expect(result['data']['getUserDailyRecords']['shower_time']).to eq(0)
+      expect(result['data']['getUserDailyRecords'][0]['date']).to eq("2022-07-09")
+      expect(result['data']['getUserDailyRecords'][0]['bagCount']).to eq(3)
+      expect(result['data']['getUserDailyRecords'][0]['containerCount']).to eq(4)
+      expect(result['data']['getUserDailyRecords'][0]['strawCount']).to eq(6)
+      expect(result['data']['getUserDailyRecords'][0]['showerTime']).to eq(0)
     end
   end
 
   def query
     <<~GQL
-    { getUserDailyRecords(id: "1")
+    { getUserDailyRecords(userId: "1")
       {
         date
-        bag_count
-        container_count
-        straw_count
-        shower_time
+        bagCount
+        containerCount
+        strawCount
+        showerTime
       }
      }
     GQL
