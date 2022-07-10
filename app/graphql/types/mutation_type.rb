31550e8954a49101ce0d72c25e449e0d9c833c39
Chris Romero <chrisleeromero1@gmail.com>
2022-07-10 15:22:14 -0600

    delete user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,5 +5,6 @@ module Types
     field :update_daily_record, mutation: Mutations::DailyRecords::UpdateDailyRecord
     field :create_user, mutation: Mutations::Users::CreateUser
     field :update_user, mutation: Mutations::Users::UpdateUser
+    field :delete_user, mutation: Mutations::Users::DeleteUser
   end
 end
