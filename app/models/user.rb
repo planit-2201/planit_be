2690dd100c2fb13a85cc7d63812d124e276b8ddd
craig cardwell <wizeduvcc@gmail.com>
2022-07-10 15:39:10 -0600

    update model, schema and add migration for users weekly average water usage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
   validates_numericality_of :flowrate
 
   has_many :daily_records, foreign_key: :user_id, dependent: :destroy
+
+  def weekly_average_shower_time(date)
+    range = find_weekly_range(date)
+    DailyRecord.where(:date => range)
+  end
+
+  private
+
+  def find_weekly_range(date)
+    end_date = DateTime.parse(date)
+    start_date = end_date-6
+    range = start_date..end_date
+    range.map {|d| d.strftime "%m-%d-%Y"}
+  end
 end
