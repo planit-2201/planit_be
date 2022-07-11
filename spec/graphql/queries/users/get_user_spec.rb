f5cd90332905055eabad41490f01ed41550260d4
Chris Romero <chrisleeromero1@gmail.com>
2022-07-11 11:22:13 -0600

    update get_user test with 7 days of data and test for average weekly water usage query

--- spec/graphql/queries/users/get_user_spec.rb
+++ spec/graphql/queries/users/get_user_spec.rb
@@ -7,16 +7,26 @@ RSpec.describe Types::QueryType do
 
       user.daily_records.create(date: "2022-07-04", shower_time: 300)
       user.daily_records.create(date: "2022-07-05", shower_time: 400)
+      user.daily_records.create(date: "2022-07-06", shower_time: 300)
       user.daily_records.create(date: "2022-07-07", shower_time: 200)
-      avg_time = user.weekly_average_shower_time("2022-07-09")
+      user.daily_records.create(date: "2022-07-08", shower_time: 200)
+      user.daily_records.create(date: "2022-07-09", shower_time: 200)
+      user.daily_records.create(date: "2022-07-10", shower_time: 200)
+      avg_time = user.weekly_average_shower_time("2022-07-11")
       allow(user).to receive(:weekly_average_shower_time).and_return(avg_time)
 
       result = PlanitBeSchema.execute(query).as_json
       expect(result["data"]["getUser"]["username"]).to eq('Mike Dao')
       expect(result["data"]["getUser"]["flowrate"]).to eq(1.8)
-      expect(result["data"]["getUser"]["weeklyAverageShowerTime"]).to eq(300.0)
+      expect(result["data"]["getUser"]["weeklyAverageShowerTime"]).to eq(250.0)
+      expect(result["data"]["getUser"]["weeklyAverageWaterUsage"]).to eq(51)
     end
   end
+
+# sum shower time from the previous 7 days - 1,800 seconds, 30 minutes
+# convert sum to minutes - 30 minutes
+# multiply flow rate (gallons/ per minute) by minutes - 30 minutes
+# return product of fr x min as a float - 51 gallons
 
   def query
     <<~GQL
