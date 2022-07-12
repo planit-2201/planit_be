2cbebb113cfc1beb99ced1a3a0ef70a916b6acb5
Kim G <xiaole.guo@yahoo.com>
2022-07-12 10:04:26 -0600

    refactor request test get_user_spec

--- spec/graphql/queries/users/get_user_spec.rb
+++ spec/graphql/queries/users/get_user_spec.rb
@@ -8,48 +8,32 @@ RSpec.describe Types::QueryType do
     dates[0..9].each {|date| FactoryBot.create(:daily_record, user_id: @user.id, date: date, shower_time: 200 )}
     dates[10..19].each {|date| FactoryBot.create(:daily_record, user_id: @user.id, date: date, shower_time: 300 )}
     dates[20..29].each {|date| FactoryBot.create(:daily_record, user_id: @user.id, date: date, shower_time: 400 )}
+    @result = PlanitBeSchema.execute(query).as_json
   end
 
   describe 'display user' do
     it "can query a single user" do
-      result = PlanitBeSchema.execute(query).as_json
-      expect(result["data"]["getUser"]["username"]).to eq('Mike Dao')
-      expect(result["data"]["getUser"]["flowrate"]).to eq(1.8)
+      expect(@result["data"]["getUser"]["username"]).to eq('Mike Dao')
+      expect(@result["data"]["getUser"]["flowrate"]).to eq(1.8)
     end
 
     it 'returns weeklyAverageShowerTime' do
-      result = PlanitBeSchema.execute(query).as_json
-      # avg_time = @user.weekly_average_shower_time("2022-07-11")
-      # allow(@user).to receive(:weekly_average_shower_time).and_return(avg_time)
-
-      expect(result["data"]["getUser"]["weeklyAverageShowerTime"]).to eq(400.0)
+      expect(@result["data"]["getUser"]["weeklyAverageShowerTime"]).to eq(400.0)
 
     end
 
     it 'returns thirtydayAverageShowerTime' do
-      result = PlanitBeSchema.execute(query).as_json
-      # thirtyday_avg_time = @user.thirtyday_average_shower_time("2022-07-11")
-      # allow(@user).to receive(:thirtyday_average_shower_time).and_return(thirtyday_avg_time)
-
-      expect(result["data"]["getUser"]["thirtydayAverageShowerTime"]).to eq(300.0)
+      expect(@result["data"]["getUser"]["thirtydayAverageShowerTime"]).to eq(300.0)
     end
 
     it 'returns weeklyAverageWaterUsage' do
-      result = PlanitBeSchema.execute(query).as_json
-      # avg_time = @user.weekly_average_shower_time("2022-07-11")
-      # allow(@user).to receive(:weekly_average_shower_time).and_return(avg_time)
-
-      expect(result["data"]["getUser"]["weeklyAverageShowerTime"]).to eq(400.0)
-      expect(result["data"]["getUser"]["weeklyAverageWaterUsage"]).to eq(12.0)
+      expect(@result["data"]["getUser"]["weeklyAverageShowerTime"]).to eq(400.0)
+      expect(@result["data"]["getUser"]["weeklyAverageWaterUsage"]).to eq(12.0)
     end
 
     it 'returns thirtydayAverageWaterUsage' do
-      result = PlanitBeSchema.execute(query).as_json
-      # thirtyday_avg_time = @user.thirtyday_average_shower_time("2022-07-11")
-      # allow(@user).to receive(:thirtyday_average_shower_time).and_return(thirtyday_avg_time)
-
-      expect(result["data"]["getUser"]["thirtydayAverageShowerTime"]).to eq(300.0)
-      expect(result["data"]["getUser"]["thirtydayAverageWaterUsage"]).to eq(9.0)
+      expect(@result["data"]["getUser"]["thirtydayAverageShowerTime"]).to eq(300.0)
+      expect(@result["data"]["getUser"]["thirtydayAverageWaterUsage"]).to eq(9.0)
     end
   end
 
