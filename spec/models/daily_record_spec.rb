85aeac1f254a6a9907d17fea6d8aedfe502ec306
Kim G <xiaole.guo@yahoo.com>
2022-07-16 09:38:58 -0600

    pass daily record model spec

--- spec/models/daily_record_spec.rb
+++ spec/models/daily_record_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe DailyRecord, type: :model do
   describe 'validations' do
     it { should validate_presence_of :date }
     it { should validate_numericality_of :bag_count }
-    it { should validate_numericality_of :container_count }
+    it { should validate_numericality_of :bottle_count }
     it { should validate_numericality_of :straw_count }
     it { should validate_numericality_of :shower_time }
   end
