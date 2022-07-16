c0ff0f9f7858dd206e1927c0da4b1abe1469336a
Kim G <xiaole.guo@yahoo.com>
2022-07-16 11:32:32 -0600

    add model tests for app-wide 30 day average shower time and water usage

--- spec/models/app_datum_spec.rb
+++ spec/models/app_datum_spec.rb
@@ -3,18 +3,25 @@ require 'rails_helper'
 RSpec.describe AppDatum, type: :model do
    describe 'instance methods' do
     before :each do
-      users = create_list(:user, 10, flowrate: 2.1)
+      @users = create_list(:user, 5, flowrate: 2.1)
       end_date = DateTime.parse(Time.now.strftime("%Y-%m-%d"))
       dates = ((end_date-29)..end_date).map {|d| d.strftime "%Y-%m-%d"}
-      users.each do |user|
+      @users.each do |user|
         create(:daily_record, user_id: user.id, date: dates[0], shower_time: 100)
         create(:daily_record, user_id: user.id, date: dates[10], shower_time: 200)
         create(:daily_record, user_id: user.id, date: dates[20], shower_time: 300)
       end
       @app_datum = AppDatum.create
     end
-    it '.returns total number of users' do
-      expect(@app_datum.user_count).to eq(10)
+    it 'returns total number of users' do
+      expect(@app_datum.user_count).to eq(5)
+    end
+
+    it 'returns 30 day average shower time by user_id across app' do 
+      expect(@app_datum.thirtyday_average_shower_time).to eq( {@users[0].id=>0.2e3, @users[1].id=>0.2e3, @users[2].id=>0.2e3, @users[3].id=>0.2e3, @users[4].id=>0.2e3})
+    end
+    it 'returns 30 day average water usage across app' do 
+      expect(@app_datum.thirtyday_average_water_usage).to eq(7.0)
     end
   end
 
