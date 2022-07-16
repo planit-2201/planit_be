a97dd2563e77993c249af1a9c8f9eff9421f11ff
Kim G <xiaole.guo@yahoo.com>
2022-07-16 11:49:53 -0600

    add user destroy all to get_app_data_spec

--- spec/graphql/queries/app_data/get_app_data_spec.rb
+++ spec/graphql/queries/app_data/get_app_data_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Types::QueryType do
   before :each do
-    app_datum = AppDatum.create
+    User.destroy_all
     users = create_list(:user, 5, flowrate: 2.1)
     end_date = DateTime.parse(Time.now.strftime("%Y-%m-%d"))
     dates = ((end_date-29)..end_date).map {|d| d.strftime "%Y-%m-%d"}
@@ -11,16 +11,14 @@ RSpec.describe Types::QueryType do
       create(:daily_record, user_id: user.id, date: dates[10], shower_time: 200)
       create(:daily_record, user_id: user.id, date: dates[20], shower_time: 300)
     end
-    @count = app_datum.user_count 
-    @water_usage = app_datum.thirtyday_average_water_usage
     @result = PlanitBeSchema.execute(query).as_json
   end
   it 'returns total number of users' do
-    expect(@result["data"]["getAppData"]["userCount"]).to eq(@count)
+    expect(@result["data"]["getAppData"]["userCount"]).to eq(5)
   end
 
   it 'returns 30 day average water usage' do
-    expect(@result["data"]["getAppData"]["thirtydayAverageWaterUsage"]).to eq(@water_usage)
+    expect(@result["data"]["getAppData"]["thirtydayAverageWaterUsage"]).to eq(7.0)
   end
 
   def query
