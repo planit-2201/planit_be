e39099bc8120bb122ebc6975d71fe631510cb549
Kim G <xiaole.guo@yahoo.com>
2022-07-16 11:40:39 -0600

    update get_app_data_spec

--- spec/graphql/queries/app_data/get_app_data_spec.rb
+++ spec/graphql/queries/app_data/get_app_data_spec.rb
@@ -1,25 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe AppDatum, type: :model do
-   describe 'instance methods' do
-    before :each do
-      users = create_list(:user, 10, flowrate: 2.1)
-      end_date = DateTime.parse(Time.now.strftime("%Y-%m-%d"))
-      dates = ((end_date-29)..end_date).map {|d| d.strftime "%Y-%m-%d"}
-      users.each do |user|
-        create(:daily_record, user_id: user.id, date: dates[0], shower_time: 100)
-        create(:daily_record, user_id: user.id, date: dates[10], shower_time: 200)
-        create(:daily_record, user_id: user.id, date: dates[20], shower_time: 300)
-      end
-      @result = PlanitBeSchema.execute(query).as_json
+RSpec.describe Types::QueryType do
+  before :each do
+    users = create_list(:user, 10, flowrate: 2.1)
+    end_date = DateTime.parse(Time.now.strftime("%Y-%m-%d"))
+    dates = ((end_date-29)..end_date).map {|d| d.strftime "%Y-%m-%d"}
+    users.each do |user|
+      create(:daily_record, user_id: user.id, date: dates[0], shower_time: 100)
+      create(:daily_record, user_id: user.id, date: dates[10], shower_time: 200)
+      create(:daily_record, user_id: user.id, date: dates[20], shower_time: 300)
     end
-    it 'returns total number of users' do
-      expect(@result["data"]["getAppData"]["userCount"]).to eq(10)
-    end
+    @result = PlanitBeSchema.execute(query).as_json
+  end
+  it 'returns total number of users' do
+    expect(@result["data"]["getAppData"]["userCount"]).to eq(10)
+  end
 
-    it 'returns 30 day average water usage' do
-      expect(@result["data"]["getAppData"]["thirtydayAverageWaterUsage"]).to eq(7.0)
-    end
+  it 'returns 30 day average water usage' do
+    expect(@result["data"]["getAppData"]["thirtydayAverageWaterUsage"]).to eq(7.0)
   end
 
   def query
