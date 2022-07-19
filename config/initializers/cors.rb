f82d3bd45786d2109f17fa9dd623b4f95301accc
Kim G <xiaole.guo@yahoo.com>
2022-07-19 11:12:11 -0600

    revert to allow all origins until code freeze

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://planit-earth.netlify.app"
-
+    # origins "https://planit-earth.netlify.app"
+    origins "*"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
